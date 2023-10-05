d67564dc23dbc4fd287f995a9c9e572673c3aa66
Akash Dubey <116936246+akashD7892@users.noreply.github.com>
2023-10-05 17:01:40 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURI = "mongodb+srv://erakash:akashD7892@cluster0.wgdzjmo.mongodb.net/rudra_inno?retryWrites=true&w=majority"
+const mongoURI = "mongodb+srv://{username}:{password}@cluster0.wgdzjmo.mongodb.net/rudra_inno?retryWrites=true&w=majority"
 
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
